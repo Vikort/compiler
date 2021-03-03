4d4cde13db17f9572a2dc032c2efbc79b9a38176
Viktor Markovec <markvik42@gmail.com>
2021-03-03 09:21:23 +0300

    feat(listener): Add MyListener, fix quotes in example

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
-from generated import EasyXMLParser, EasyXMLLexer, EasyXMLListener, EasyXMLVisitor
+from generated import EasyXMLParser, EasyXMLLexer
+from listener import MyListener
 from antlr4 import *
 
 
 def main():
-    input_stream = InputStream("print(doc.root);")
+    input_stream = FileStream("langExample.el")
     lexer = EasyXMLLexer.EasyXMLLexer(input_stream)
     stream = CommonTokenStream(lexer)
     parser = EasyXMLParser.EasyXMLParser(stream)
     tree = parser.xml()
     print(tree.toStringTree(None, parser))
     walker = ParseTreeWalker()
-    walker.walk(EasyXMLListener.EasyXMLListener(), tree)
-    return None
+    walker.walk(MyListener.MyListener(), tree)
 
 
 if __name__ == '__main__':
